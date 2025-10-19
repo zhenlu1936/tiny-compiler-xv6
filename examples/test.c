47fc77e75a6f78ded0d0625e454a8864239845d7
zhenlu <zhenlu1936@gmail.com>
2025-10-19 12:16:29 +0000

    v1.0

--- examples/test.c
+++ examples/test.c
@@ -10,12 +10,11 @@ int main()
 {
 	int a,b,c;
 	output "waiting for input\n";
-	// input a;
-	// input b;
-	// c=add(a,b);
-	c=add(1,2);
+	input a;
+	input b;
+	c=add(a,b);
 	output "going to output\n";
-	// output c;
+	output c;
 	output "\n";
 	return 0;
 }
